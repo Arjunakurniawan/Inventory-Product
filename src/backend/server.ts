8f51924aba498d7ded6a1c0d19096ec92c097dd6
ArjunaKurniawan <arjunakurniawan686@gmail.com>
2025-02-22 12:17:04 +0700

    refactor: update type definitions and enhance database schema for products, categories, warehouses, and transactions

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
+import { Category, PrismaClient, Warehouse } from "@prisma/client";
 
-import { product, category, warehouse, transaction } from "./typing/model";
+import {
+  ProductRequest,
+  Product,
+  ApiResponse,
+  CategoryRequest,
+  WarehouseRequest,
+  Transaction,
+  TransactionWithItems,
+  TransactionRequest,
+} from "./typing/model";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -11,287 +20,387 @@ app.use(express.json());
 dotenv.config();
 
 // CRUD product
-app.get("/product", async (_: Request, res: Response) => {
+app.get<string, null, ApiResponse<Product[]>>("/product", async (_, res) => {
   try {
-    const GetProducts =
+    const products =
       (await prisma.product.findMany({
         where: {
           deletedAt: null,
         },
         include: {
-          category: {
-            select: {
-              name: true,
-            },
-          },
+          category: true,
+          warehouse: true,
         },
       })) || [];
-    res.status(200).json(GetProducts);
+
+    res.status(200).json({ data: products, message: "success" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error Get Products" });
+    res.status(500).json({ data: [], message: "Error Get Products" });
   }
 });
 
-app.post("/product", async (req: Request, res: Response) => {
-  try {
-    const {
-      name,
-      description,
-      price,
-      image,
-      stock,
-      warehouseId,
-      categoryId,
-    }: product = req.body;
+app.get<string, { id: string }, ApiResponse<Product | null>>(
+  "/product/:id",
+  async (req, res) => {
+    try {
+      const product =
+        (await prisma.product.findFirstOrThrow({
+          where: {
+            id: parseInt(req.params.id),
+            deletedAt: null,
+          },
+          include: {
+            category: true,
+            warehouse: true,
+          },
+        })) || [];
 
-    if (!warehouseId || !categoryId) {
-      res.status(400).json({ error: "warehouseId not found" });
+      res.status(200).json({ data: product, message: "success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Get Product" });
     }
-
-    await prisma.product.create({
-      data: {
-        name,
-        description,
-        price,
-        image,
-        warehouseId,
-        categoryId,
-        stock,
-      },
-    });
-    res.status(201).json({ message: "Product created successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Create Product" });
   }
-});
+);
 
-app.delete("/product/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    await prisma.product.update({
-      where: { id: parseInt(id) },
-      data: {
-        deletedAt: new Date(),
-      },
-    });
-    console.log("product soft deleted successfully");
-    res.status(204).json({ message: "Product deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Delete Product" });
+app.post<string, null, ApiResponse<null>, ProductRequest>(
+  "/product",
+  async (req, res) => {
+    try {
+      await prisma.product.create({
+        data: req.body,
+      });
+      res
+        .status(201)
+        .json({ data: null, message: "Product created successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Create Product" });
+    }
   }
-});
+);
+
+app.put<string, { id: string }, ApiResponse<null>, ProductRequest>(
+  "/product/:id",
+  async (req, res) => {
+    try {
+      await prisma.product.update({
+        data: req.body,
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      res.status(200).json({ data: null, message: "success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Get Product" });
+    }
+  }
+);
+
+app.delete<string, { id: string }, ApiResponse<null>>(
+  "/product/:id",
+  async (req, res) => {
+    try {
+      await prisma.product.update({
+        where: { id: parseInt(req.params.id) },
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+      console.log("product soft deleted successfully");
+      res
+        .status(204)
+        .json({ data: null, message: "Product deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Delete Product" });
+    }
+  }
+);
 
 // end product
 
 // CRUD category
-app.get("/category", async (_: Request, res: Response) => {
+app.get<string, null, ApiResponse<Category[]>>("/category", async (_, res) => {
   try {
-    const GetCategories =
+    const categories =
       (await prisma.category.findMany({
         where: {
           deletedAt: null,
         },
       })) || [];
-    console.log("fetching category", GetCategories);
-    res.status(200).json(GetCategories);
+    res.status(200).json({ data: categories, message: null });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error Get Categories" });
+    res.status(500).json({ data: [], message: "Error Get Categories" });
   }
 });
 
-app.post("/category", async (req: Request, res: Response) => {
-  try {
-    const { name }: category = req.body;
-    await prisma.category.create({
-      data: {
-        name,
-      },
-    });
-    res.status(201).json({ message: "Success Created" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Create Category" });
+app.post<string, null, ApiResponse<null>, CategoryRequest>(
+  "/category",
+  async (req, res) => {
+    try {
+      await prisma.category.create({
+        data: req.body,
+      });
+      res.status(201).json({ data: null, message: "Success Created" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Create Category" });
+    }
   }
-});
+);
 
-app.delete("/category/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    await prisma.category.update({
-      where: { id: parseInt(id) },
-      data: {
-        deletedAt: new Date(),
-      },
-    });
-    console.log("category soft deleted");
-    res.status(200).json({ message: "Success Delete" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Delete Category" });
+app.delete<string, { id: string }, ApiResponse<null>>(
+  "/category/:id",
+  async (req: Request, res: Response) => {
+    try {
+      await prisma.category.update({
+        where: { id: parseInt(req.params.id) },
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+      console.log("category soft deleted");
+      res.status(200).json({ data: null, message: "Success Delete" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, error: "Error Delete Category" });
+    }
   }
-});
+);
 
-app.put("/category/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { name }: category = req.body;
-    await prisma.category.update({
-      where: { id: parseInt(id) },
-      data: {
-        name,
-      },
-    });
-    console.log("product soft deleted");
-    res.status(200).json({ message: "Success Update" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Update Category" });
+app.put<string, { id: string }, ApiResponse<null>, CategoryRequest>(
+  "/category/:id",
+  async (req, res) => {
+    try {
+      await prisma.category.update({
+        where: { id: parseInt(req.params.id) },
+        data: req.body,
+      });
+      console.log("category soft deleted");
+      res.status(200).json({ data: null, message: "Success Update" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Update Category" });
+    }
   }
-});
+);
 
 //end category
 
 //CRUD warehouse
-app.get("/warehouse", async (_: Request, res: Response) => {
-  try {
-    const GetWarehouses =
-      (await prisma.warehouse.findMany({
+app.get<string, null, ApiResponse<Warehouse[]>>(
+  "/warehouse",
+  async (_, res) => {
+    try {
+      const warehouse =
+        (await prisma.warehouse.findMany({
+          where: {
+            deletedAt: null,
+          },
+        })) || [];
+      res.status(200).json({ data: warehouse, message: "success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: [], message: "Error Get Warehouses" });
+    }
+  }
+);
+
+app.post<string, null, ApiResponse<null>, WarehouseRequest>(
+  "/warehouse",
+  async (req, res) => {
+    try {
+      await prisma.warehouse.create({
+        data: req.body,
+      });
+      res.status(201).json({ data: null, message: "Success Created" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Create Warehouse" });
+    }
+  }
+);
+
+app.delete<string, { id: string }, ApiResponse<null>, WarehouseRequest>(
+  "/warehouse/:id",
+  async (req, res) => {
+    try {
+      await prisma.warehouse.update({
         where: {
-          deletedAt: null,
+          id: parseInt(req.params.id),
         },
-      })) || [];
-    res.status(200).json(GetWarehouses);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Get Warehouses" });
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+      res.status(200).json({ data: null, message: "Category is Soft Delete" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Delete Warehouse" });
+    }
   }
-});
+);
 
-app.post("/warehouse", async (req: Request, res: Response) => {
-  try {
-    const { name, address, phone }: warehouse = req.body;
-    await prisma.warehouse.create({
-      data: {
-        name,
-        address,
-        phone,
-      },
-    });
-    res.status(201).json({ message: "Success Created" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Create Warehouse" });
+app.put<string, { id: string }, ApiResponse<null>>(
+  "/warehouse/:id",
+  async (req, res) => {
+    try {
+      await prisma.warehouse.update({
+        where: { id: parseInt(req.params.id) },
+        data: req.body,
+      });
+      res.status(200).json({ data: null, message: "Success Update" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Update Warehouse" });
+    }
   }
-});
-
-app.delete("/warehouse/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    await prisma.warehouse.update({
-      where: {
-        id: parseInt(id),
-      },
-      data: {
-        deletedAt: new Date(),
-      },
-    });
-    res.status(200).json({ message: "Category is Soft Delete" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Delete Warehouse" });
-  }
-});
-
-app.put("/warehouse/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { name, address, phone }: warehouse = req.body;
-    await prisma.warehouse.update({
-      where: { id: parseInt(id) },
-      data: {
-        name,
-        address,
-        phone,
-      },
-    });
-    res.status(200).json({ message: "Success Update" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Update Warehouse" });
-  }
-});
+);
 //end warehouse
 
 //CRUD Transaction
-app.get("/transaction", async (_: Request, res: Response) => {
-  try {
-    const getTransaction = await prisma.transaction.findMany({
-      where: {
-        deletedAt: null,
-      },
-    });
-    res.status(200).json(getTransaction);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Get Transactions" });
-  }
-});
-
-app.get("/transaction/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const getTransaction =
-      (await prisma.transaction.findUnique({
+app.get<string, null, ApiResponse<Transaction[]>>(
+  "/transaction",
+  async (_, res) => {
+    try {
+      const transactions = await prisma.transaction.findMany({
         where: {
-          id: parseInt(id),
+          deletedAt: null,
+        },
+      });
+      res.status(200).json({ data: transactions, message: "success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: [], message: "Error Get Transactions" });
+    }
+  }
+);
+
+app.get<string, { id: string }, ApiResponse<TransactionWithItems | null>>(
+  "/transaction/:id",
+  async (req, res) => {
+    try {
+      const transaction = await prisma.transaction.findFirstOrThrow({
+        where: {
+          id: parseInt(req.params.id),
           deletedAt: null,
         },
         include: {
-          transactionItem: {
+          items: {
             include: {
               product: {
-                select: {
-                  name: true,
-                  price: true,
+                include: {
+                  category: true,
+                  warehouse: true,
                 },
               },
             },
           },
         },
-      })) || [];
-    console.log(getTransaction);
-    res.status(200).json(getTransaction);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Get Transactions" });
+      });
+      res.status(200).json({ data: transaction, message: "success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Get Transactions" });
+    }
   }
-});
+);
 
-app.post("/transaction", async (req: Request, res: Response) => {
-  try {
-    const { type, productId, quantity }: transaction = req.body;
-
-    await prisma.transaction.create({
-      data: {
-        type,
-        transactionItem: {
-          create: {
-            productId,
-            quantity,
+app.post<string, null, ApiResponse<null>, TransactionRequest>(
+  "/transaction",
+  async (req, res) => {
+    try {
+      await prisma.transaction.create({
+        data: {
+          ...req.body,
+          items: {
+            createMany: {
+              data: req.body.items,
+            },
           },
         },
+      });
+
+      console.log(req.body.items);
+      console.log(req.body.type);
+
+      // const transaction = await prisma.transaction.create({
+      //   data: {
+      //     type: req.body.type,
+      //   },
+      // });
+
+      // await prisma.transactionItem.createMany({
+      //   data: req.body.items.map((item) => ({
+      //     transactionId: transaction.id,
+      //     productId: item.productId,
+      //     price: item.price,
+      //     quantity: item.quantity,
+      //   })),
+      // });
+
+      res.status(201).json({ data: null, message: "Success Created" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Create Transaction" });
+    }
+  }
+);
+
+app.delete("/transaction/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await prisma.transaction.update({
+      where: { id: parseInt(id) },
+      data: {
+        deletedAt: new Date(),
       },
     });
-    res.status(201).json({ message: "Success Created" });
+    res.status(200).json({ message: "transaction soft deleted" });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error Create Transaction" });
+    console.log(error);
+    res.status(500).json({ error: "Error Delete Transaction" });
   }
 });
+
+app.put<string, { id: string }, ApiResponse<null>, TransactionRequest>(
+  "/transaction/:id",
+  async (req, res) => {
+    try {
+      await prisma.transaction.update({
+        where: { id: parseInt(req.params.id) },
+        data: {
+          type: req.body.type,
+        },
+      });
+
+      await prisma.transactionItem.deleteMany({
+        where: {
+          transactionId: parseInt(req.params.id),
+        },
+      });
+
+      await prisma.transactionItem.createMany({
+        data: req.body.items.map((item) => ({
+          transactionId: parseInt(req.params.id),
+          productId: item.productId,
+          price: item.price,
+          quantity: item.quantity,
+        })),
+      });
+
+      res.status(200).json({ data: null, message: "Success Update" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ data: null, message: "Error Update Transaction" });
+    }
+  }
+);
+
 //end Transaction
 
 const connectPrisma = async () => {
--- src/backend/typing/model.ts
+++ src/backend/typing/model.ts
@@ -1,25 +1,83 @@
-export type product = {
+export type Product = {
+  id: number;
   name: string;
-  description?: string;
+  description: string | null;
   price: number;
   stock: number;
-  image?: string;
+  image: string | null;
   warehouseId: number;
+  warehouse: Warehouse | null;
   categoryId: number;
+  category: Category | null;
 };
 
-export type category = {
+export type Category = {
+  id: number;
   name: string;
 };
 
-export type warehouse = {
+export type Warehouse = {
+  id: number;
   name: string;
   address: string;
   phone: string;
 };
 
-export type transaction = {
-  type: string;
+// string union vs enum di typescript
+type TransactionType = "INCOMING" | "OUTCOMING";
+
+export type Transaction = {
+  id: number;
+  type: TransactionType;
+  createdAt: Date;
+};
+
+export type TransactionWithItems = {
+  id: number;
+  type: TransactionType;
+  createdAt: Date;
+  items: TransactionItem[];
+};
+
+export type TransactionItem = {
   productId: number;
+  transactionId: number;
   quantity: number;
+  price: number;
+  product: Product | null;
+};
+
+export type ProductRequest = {
+  name: string;
+  description: string | null;
+  price: number;
+  stock: number;
+  image: string | null;
+  warehouseId: number;
+  categoryId: number;
+};
+
+export type CategoryRequest = {
+  name: string;
+};
+
+export type WarehouseRequest = {
+  name: string;
+  address: string;
+  phone: string;
+};
+
+export type TransactionRequest = {
+  type: TransactionType;
+  items: {
+    productId: number;
+    quantity: number;
+    price: number;
+  }[];
+};
+
+//response JSON
+export type ApiResponse<T> = {
+  data: T;
+  message: string | null;
 };
